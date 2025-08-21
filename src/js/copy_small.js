6c4be0f3a0caa8f18fd0db050f125df9be38969b
nexeption <nexeption@gmail.com>
2025-08-21 21:25:49 +0300

    ok

--- src/js/copy_small.js
+++ src/js/copy_small.js
@@ -1,32 +1,16 @@
 export function initCopySmall() {
-   console.log('initCopySmall: скрипт инициализирован');
-
    document.querySelectorAll('a.copy').forEach((link) => {
-      console.log('initCopySmall: найден элемент', link);
-
       link.addEventListener('click', (event) => {
          event.preventDefault();
          event.stopPropagation();
 
-         // Убираем хэш, если он появился в URL
+         // убираем хэш из URL
          history.pushState('', document.title, window.location.pathname + window.location.search);
 
-         console.log('initCopySmall: клик по элементу', link);
-
          const href = link.getAttribute('href');
-         if (!href) {
-            console.warn('initCopySmall: href пустой');
-            return;
-         }
+         if (!href) return;
 
-         navigator.clipboard
-            .writeText(href)
-            .then(() => {
-               console.log(`initCopySmall: скопировано в буфер — ${href}`);
-            })
-            .catch((err) => {
-               console.error('initCopySmall: ошибка копирования', err);
-            });
+         navigator.clipboard.writeText(href).catch(() => {});
       });
    });
 }
